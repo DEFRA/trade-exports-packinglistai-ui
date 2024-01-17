ae80914b6ce930780cdc94aedb1b2c83b7640732
Jim Davenport <jim@dynamicmonkeys.com>
2024-01-17 16:22:17 +0000

    Fixed SonarCloud formatting issues

--- app/routes/packing-list-upload.js
+++ app/routes/packing-list-upload.js
@@ -5,22 +5,17 @@ module.exports = {
   method: 'GET',
   path: '/packing-list-upload',
   options: {
-    handler: async (request, h) => {
+    handler: async (_request, h) => {
       const uploadsDir = path.join(process.cwd(), '/app/dist/uploads/')
 
-      // 1: See whether there's an uploads dir yet created or not
       if (!fs.existsSync(uploadsDir)) {
-        // 2: If not there, create it
         await fs.promises.mkdir(uploadsDir)
       }
 
-      // 3: It's there, read the stack of files
       const fileList = await fs.promises
         .readdir(uploadsDir)
-        .then((files) => {
-          return files
-        })
-        .catch((err) => {
+        .then(files => files)
+        .catch(err => {
           throw new Error(err)
         })
 
