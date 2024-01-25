3496ad7d763fd659975dd2c951923d21bbf79b69
Jim Davenport <jim@dynamicmonkeys.com>
2024-01-25 18:29:03 +0000

    Mopping up views.js test gap

--- test/integration/narrow/app/app.test.js
+++ test/integration/narrow/app/app.test.js
@@ -14,3 +14,34 @@ describe('Entry point test', () => {
     expect(mockStart.mock.calls.length).toBe(1)
   })
 })
+
+describe('Check prepare views function works', () => {
+  const path = require('path')
+  const nunjucks = require('nunjucks')
+  const { options } = require('../../../../app/plugins/views')
+
+  jest.mock('nunjucks')
+  jest.mock('path')
+
+  test('should configure nunjucks environment', () => {
+    const mockOptions = {
+      relativeTo: '/relative/path',
+      path: '/path',
+      compileOptions: {}
+    }
+    const next = jest.fn()
+
+    path.join.mockImplementation((...args) => args.join('/'))
+
+    options.engines.njk.prepare(mockOptions, next)
+
+    expect(nunjucks.configure).toHaveBeenCalledWith([
+      '/relative/path//path',
+      'app/views',
+      'node_modules/govuk-frontend/'
+    ], {
+      autoescape: true
+    })
+    expect(next).toHaveBeenCalled()
+  })
+})
