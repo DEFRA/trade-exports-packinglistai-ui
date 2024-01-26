62589625227258348fc8dc84b93fc9312b29ce52
Jim Davenport <jim@dynamicmonkeys.com>
2024-01-26 09:21:13 +0000

    Sonar coverage for uploads

--- test/integration/narrow/routes/upload.test.js
+++ test/integration/narrow/routes/upload.test.js
@@ -1,24 +1,89 @@
-const createServer = require('../../../../app/server')
+const fs = require('fs')
+const path = require('path')
+const { method, path: routePath, options } = require('../../../../app/routes/upload')
 
-describe.skip('Upload view test', () => {
-  let server
+jest.mock('fs')
+jest.mock('path')
 
-  beforeEach(async () => {
-    server = await createServer()
-    await server.initialize()
+describe('upload', () => {
+  test('should have a POST method', () => {
+    expect(method).toBe('POST')
   })
 
-  afterEach(async () => {
-    await server.stop()
+  test('should have the correct path', () => {
+    expect(routePath).toBe('/upload')
   })
 
-  test('GET /upload route returns 200', async () => {
-    const options = {
-      method: 'GET',
-      url: '/upload'
+  test('should handle file upload', async () => {
+    const h = {
+      redirect: jest.fn()
     }
 
-    const response = await server.inject(options)
-    expect(response.statusCode).toBe(200)
+    const request = {
+      payload: {
+        fileUpload: {
+          hapi: {
+            filename: 'test.txt'
+          },
+          _data: Buffer.from('test')
+        }
+      }
+    }
+
+    fs.promises = {
+      writeFile: jest.fn().mockResolvedValue()
+    }
+
+    path.join.mockReturnValue('/app/dist/uploads/')
+
+    await options.handler(request, h)
+
+    expect(fs.promises.writeFile).toBeCalledWith('/app/dist/uploads/test.txt', Buffer.from('test'))
+  })
+
+  test('should redirect if no filename', async () => {
+    const h = {
+      redirect: jest.fn()
+    }
+
+    const request = {
+      payload: {
+        fileUpload: {
+          hapi: {
+            filename: ''
+          },
+          _data: Buffer.from('test')
+        }
+      }
+    }
+
+    await options.handler(request, h)
+
+    expect(h.redirect).toBeCalledWith('/packing-list-upload')
+  })
+
+  test('should throw an error if writing the file fails', async () => {
+    const h = {
+      redirect: jest.fn()
+    }
+
+    const request = {
+      payload: {
+        fileUpload: {
+          hapi: {
+            filename: 'test.txt'
+          },
+          _data: Buffer.from('test')
+        }
+      }
+    }
+
+    fs.promises = {
+      writeFile: jest.fn().mockRejectedValue(new Error('Test error'))
+    }
+
+    path.join.mockReturnValue('/app/dist/uploads/')
+
+    await expect(options.handler(request, h)).rejects.toThrow('Test error')
   })
 })
